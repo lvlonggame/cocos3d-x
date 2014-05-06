539785c51d74631adfad49cc2f3d3eeaadebac42
lvlong <lvlonggame@gmail.com>
2014-05-06 17:42:28 +0800

    move EngineDemo to samples

--- samples/EngineDemo/proj.android/jni/Android.mk
+++ samples/EngineDemo/proj.android/jni/Android.mk
@@ -44,7 +44,7 @@ LOCAL_SRC_FILES := main.cpp \
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
 LOCAL_CFLAGS   := -D__ANDROID__\
-                  -I"$(LOCAL_PATH)/../../../cocos3d"
+                  -I"$(LOCAL_PATH)/../../../../cocos3dx"
 
 LOCAL_CPPFLAGS := -std=c++11 -pthread -frtti -fexceptions
 
@@ -55,5 +55,5 @@ LOCAL_LDLIBS :=  -landroid -lEGL -lGLESv2
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,2d) 
-$(call import-module,cocos3d)
+$(call import-module,cocos3dx)
 $(call import-module,chipmunk)
